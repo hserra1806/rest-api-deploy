be46fc2fcb302b7977981dcf4d51e6db5277a063
Henry Serra <hserra.morejon@gmail.com>
2024-02-19 13:08:51 -0500

    deploy

---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require('express')
+const crypto = require('crypto')
+const cors = require('cors')
+
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: (origin, cb) => {
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:3000',
+            'http://localhost:8080',
+            'http://localhost:1234',
+            'https://movies.com'
+        ]
+
+        if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+            return cb(null, true)
+        }
+
+        return cb(new Error('Not allowed by CORS'))
+    }
+}))
+app.disable('x-powered-by')
+
+app.get('/movies', (req, res) => {
+    const { genre } = req.query 
+    if (genre) {
+        const filteredMovies = movies.filter (
+            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies)
+    }
+    res.json({ movies })
+})
+
+app.get('movies/:id', (req, res) => {
+    const { id } = req.params
+    const movie = movies.find((movie) => movie.id === id)
+
+    if (movie) return res.json(movie)
+
+    res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+    const result = validateMovie(req.body)
+
+    if (result.error) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const newMovie = {
+        id: crypto.randomUUID(),
+        ...result.data
+    }
+
+    // esto no seria REST, porque estamos modificando el estado del servidor
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie) // actualizar la caché del cliente
+})
+
+app.patch('/movies/:id', (req, res) => {
+    const result = validatePartialMovie(req.body)
+
+    if(!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const { id } = req.params
+    const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    const updateMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    movies[movieIndex] = updateMovie
+
+    return res.json(updateMovie)
+
+})
+
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    movies.splice(movieIndex, 1)
+
+    res.status(204)
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+})
